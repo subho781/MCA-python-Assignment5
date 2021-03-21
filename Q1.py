0a4587a512c52b9a2c4fccca26e10aab282e1f28
Subhajit Sarkar <77772768+subho781@users.noreply.github.com>
2021-03-21 10:52:10 +0530

    upload

---
+++ Q1.py
@@ -0,0 +1,10 @@
+# Build a list containing five integer zeros(using two methods)
+
+def L1(number):
+    return [0] * number
+list = L1(5)
+print('L1=',list)
+for i in range(6):
+    L2=[0] * i
+print('L2=',L2)
+
